acdf5c5f70e067b16750ae8bf7998c6d1a079835
Sovremeny-eth <sovremeny.tehnology@gmail.com>
2024-04-02 23:23:30 +0300

    feat: settings

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -4,7 +4,6 @@ import '@nomicfoundation/hardhat-foundry';
 import '@nomiclabs/hardhat-ethers';
 import 'hardhat-deploy';
 import '@typechain/hardhat';
-import './tasks/test_userOp';
 
 dotenv.config();
 
